2055e374f0b4aca1ae21e2faf8eb946c2612f5ab
andrepavanelli7 <pavanellidev@gmail.com>
2023-01-02 14:12:17 -0300

    estudos 02/01/23

---
+++ 038 - Manipulando_atributos/001.js
@@ -0,0 +1,22 @@
+const input = document.getElementById('input')
+
+document.getElementById('value').addEventListener('click', function (){
+   input.value = input.value === '' ? 'Olá, mundo!' : ''
+})
+
+document.getElementById('type').addEventListener('click', function(){
+   input.type = input.type !== 'radio' ? 'radio' : 'text'
+})
+
+document.getElementById('placeholder').addEventListener('click', function() {
+   input.placeholder = input.placeholder === '' ? 'Digite algo...' : ''
+})
+
+document.getElementById('disable').addEventListener('click', function() {
+   input.setAttribute('disabled', !input.disabled)
+})
+
+document.getElementById('data').addEventListener('click', function() {
+   const data = input.dataset.something
+   console.log('O valor do atributo data-something é: ' + data)
+})
