9152101b0a83fc0b6139c6da227d0d72634128b7
andrepavanelli7 <pavanellidev@gmail.com>
2023-01-25 08:46:58 -0300

    estudos 25/01/23

--- 054-exercicio-17-webpack/src/index.js
+++ 054-exercicio-17-webpack/src/index.js
@@ -3,3 +3,7 @@ import './styles/index.css'
 const hello = () => console.log(`It works! ${1+1}`)
 
 hello()
+
+document.getElementById('emoji').addEventListener('click', function(){
+  alert('hello')
+})
--- 054-exercicio-17-webpack/webpack.config.js
+++ 054-exercicio-17-webpack/webpack.config.js
@@ -1,4 +1,12 @@
+const path = require('path')
+
 module.exports = {
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    compress: true,
+  },
   entry: {
     index: './src/index.js'
   },
