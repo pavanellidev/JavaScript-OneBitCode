b015235a43336fdbe51e7177cc40e49adb95613c
andrepavanelli7 <pavanellidev@gmail.com>
2023-01-30 07:39:09 -0300

    estudos 30/01/23

--- 0003-part3/061-exercicio20/Component.js
+++ 0003-part3/061-exercicio20/Component.js
@@ -5,10 +5,24 @@ export class Component {
     this.tag = tag
     this.parent = parent
     this.options = options
+    this.build()
+  }
 
+  getElement() {
+    return this.#element
   }
 
   build() {
-    
+    this.#element = document.createElement(this.tag)
+    Object.assign(this.#element, this.options)
+    return this
+  }
+
+  render() {
+    if(this.parent instanceof Component) {
+      this.parent.getElement().append(this.#element)
+    } else {
+      document.querySelector(this.parent).append(this.#element)
+    }
   }
 }
