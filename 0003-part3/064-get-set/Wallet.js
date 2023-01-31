920d7e1e84b5cbe51c2475c3f051d4e8ef518bd0
andrepavanelli7 <pavanellidev@gmail.com>
2023-01-31 15:00:11 -0300

    estudos 31/01/23

---
+++ 0003-part3/064-get-set/Wallet.js
@@ -0,0 +1,20 @@
+class Wallet {
+  #amount
+  #username
+
+  constructor() {
+    this.#amount = 100.99 * 100
+  }
+
+  get amount() {
+    return this.#amount / 100
+  }
+
+  set username(newUsername) {
+    this.#username = newUsername
+  }
+}
+
+const myWallet = new Wallet()
+
+console.log(myWallet.amount)
